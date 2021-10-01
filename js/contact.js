b78cbbc9de4c1baadbbcce72ef89fc64d8125691
Parth Patel <prpatel@udel.edu>
2021-09-30 23:29:57 -0400

    trying again

--- js/contact.js
+++ js/contact.js
@@ -4,7 +4,7 @@ window.onload = function() {
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
+        emailjs.sendForm('service_wmh4peq', 'template_bfbish3', this)
             .then(function() {
                 console.log('SUCCESS!');
             }, function(error) {
